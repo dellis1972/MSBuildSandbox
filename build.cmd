8b2331398d67fc2f9b2baa935a95e5a013955a60
Dean Ellis <dellis1972@googlemail.com>
2021-06-29 13:01:35 +0100

    Add Windows Build Scripts

---
+++ build.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+FOR /F "tokens=1 delims=" %%F IN ('vswhere.cmd') DO SET result=%%F
+2>NUL CALL "%result%\Common7\Tools\VsDevCmd.bat"
+IF ERRORLEVEL 1 CALL:FAILED_CASE
+
+msbuild MSBuildSandbox.sln /restore /t:Build /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary
